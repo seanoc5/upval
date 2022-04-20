aa4ef40edcc587e6608e346082612ef680ba35d6
Sean O'Connor <seanoc5@gmail.com>
2022-04-20 15:06:29 -0400

    minor func name change

--- src/main/groovy/com/lucidworks/ps/upval/mapping/LdapAclsTransformer.groovy
+++ src/main/groovy/com/lucidworks/ps/upval/mapping/LdapAclsTransformer.groovy
@@ -5,21 +5,22 @@ import org.apache.log4j.Logger
 class LdapAclsTransformer extends ObjectTransformer {
     Logger log = Logger.getLogger(this.class.name);
 
-    LdapAclsTransformer() {
+    LdapAclsTransformer(Map srcMap, Map destMap, Map transConfig, String separator= '/') {
+        super(srcMap, destMap, transConfig, separator)
         log.info "Construct new LdapAclsTransformer..."
     }
 
     def test(String templatePath, String sourcePath, Map templateObject, Map sourceObject) {
         log.info "Testing 'transform' function (more to come): $sourcePath -> $templatePath (not showing objects...)"
-        def src = getByMapPath(sourcePath, sourceObject)
-        def destVal = getByMapPath(templatePath, templateObject)
+        def src = getValueByMapPath(sourcePath, sourceObject)
+        def destVal = getValueByMapPath(templatePath, templateObject)
         def rc = setByMapPath(templatePath, src, templateObject)
     }
 
     def startLinks(String templatePath, String sourcePath, Map templateObject, Map sourceObject) {
         log.info "Testing 'transform' function (more to come): $sourcePath -> $templatePath (not showing objects...)"
-        def src = getByMapPath(sourcePath, sourceObject)
-        def destVal = getByMapPath(templatePath, templateObject)
+        def src = getValueByMapPath(sourcePath, sourceObject)
+        def destVal = getValueByMapPath(templatePath, templateObject)
         def rc = setByMapPath(templatePath, src, templateObject)
     }
 }
