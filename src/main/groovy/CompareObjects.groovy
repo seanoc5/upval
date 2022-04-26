9a73cbc443c73552a61049f6d60353d4f64b1bbb
Sean O'Connor <seanoc5@gmail.com>
2022-04-26 11:12:15 -0400

    improving simple transfor--including sample json to parse consistently

--- src/main/groovy/CompareObjects.groovy
+++ src/main/groovy/CompareObjects.groovy
@@ -1,5 +1,6 @@
 package misc
 
+import com.lucidworks.ps.upval.mapping.SimpleTransform
 import de.danielbechler.diff.ObjectDifferBuilder
 import de.danielbechler.diff.node.DiffNode
 import de.danielbechler.diff.node.Visit
@@ -13,40 +14,21 @@ Logger log = Logger.getLogger(this.class.name);
 
 log.info "Starting script ${this.class.name}..."
 
-String origJson = '''
-{
-    'topLeaf':'one',
-    'topParent': { 
-        'secondLeaf':'two-one'
-    }
-}    
-'''
-
-String alteredJson = '''
-{
-    "topLeaf":"one",
-    "topParent": { 
-        "firstLeaf":"two-one-new",
-        "addedLeaf":"found in orginal alteredJson source"        
-    }
-}    
-'''
 
 JsonSlurper jsonSlurper = new JsonSlurper()
-Map origMap = jsonSlurper.parseText(origJson)
-Map alteredMap = jsonSlurper.parseText(alteredJson)
+Map origMap = jsonSlurper.parseText(SimpleTransform.origJson)
+Map alteredMap = jsonSlurper.parseText(SimpleTransform.alteredJson)
 
 
-DiffNode root = ObjectDifferBuilder.buildDefault().compare(origMap, templateJson)
+DiffNode root = ObjectDifferBuilder.buildDefault().compare(origMap, alteredMap)
 log.info "Diff node: $root"
 root.visit(new DiffNode.Visitor() {
     public void node(DiffNode node, Visit visit) {
         final Object baseValue = node.canonicalGet(origMap);
-        final Object workingValue = node.canonicalGet(templateJson);
+        final Object workingValue = node.canonicalGet(alteredMap);
         log.info "Node path: ${node.getPath()}  state: ${node.getState()}: base(old/src) value: $baseValue -- working value (template): $workingValue"
     }
 })
-log.info "test: " + templateJson.properties.crawlDBType
 
 log.info "Done...?"
 
