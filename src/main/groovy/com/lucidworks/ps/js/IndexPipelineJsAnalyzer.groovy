842e93587f3aa646391fe61f68d6aef8fa0678e1
Sean O'Connor <seanoc5@gmail.com>
2022-05-18 11:51:52 -0400

    moving functionality to class

---
+++ src/main/groovy/com/lucidworks/ps/js/IndexPipelineJsAnalyzer.groovy
@@ -0,0 +1,14 @@
+package com.lucidworks.ps.js
+
+/**
+ * @author :    sean
+ * @mailto :    seanoc5@gmail.com
+ * @created :   5/18/22, Wednesday
+ * @description:
+ */
+
+/**
+ * helper class to analyze javascript in index pipeline stages (regular or managed)
+ */
+class IndexPipelineJsAnalyzer {
+}
