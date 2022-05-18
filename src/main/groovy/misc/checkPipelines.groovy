eb397adcc3259e0f7bc7b5cf49da48c3ff7c6523
Sean O'Connor <seanoc5@gmail.com>
2022-05-18 11:51:40 -0400

    refactoring

--- src/main/groovy/misc/checkPipelines.groovy
+++ src/main/groovy/misc/checkPipelines.groovy
@@ -1,12 +1,13 @@
 package misc
 
 import com.lucidworks.ps.clients.FusionClient
+import com.lucidworks.ps.js.PipelineJsAnalyzer
 import com.lucidworks.ps.upval.ExtractFusionObjectsForIndexing
 import com.lucidworks.ps.upval.FusionClientArgParser
 import groovy.cli.picocli.OptionAccessor
 import org.apache.log4j.Logger
 
-Logger log = Logger.getLogger(this.class.name);
+Logger log = Logger.getLogger(this.class.name)
 
 OptionAccessor options = FusionClientArgParser.parse(this.class.name, args)
 
@@ -17,43 +18,41 @@ File srcJson = fusionClient.objectsJsonFile
 Map parsedMap = ExtractFusionObjectsForIndexing.readObjectsJson(srcJson)
 Map sourceFusionOjectsMap = parsedMap.objects
 
-List<Map<String,Object>> indexPipelines = sourceFusionOjectsMap.indexPipelines
-def stages = indexPipelines.collect {it.stages}.flatten()
-def skipped = stages.findAll{it.skip}
+List<Map<String, Object>> indexPipelines = sourceFusionOjectsMap.indexPipelines
+def stages = indexPipelines.collect { it.stages }.flatten()
+def jsStages = PipelineJsAnalyzer.collectJsStages(stages)
 
-def indexStageTypes = stages.groupBy{it.type}
-log.info "Index pipeline stage types:\n" + indexStageTypes.collect {String key, def val -> "${val.size()} :: $key"}.join('\n')
+// bulk list (all index & query JS stage) of javascript code lines, will be filtered and analyzed at the end
+List<String> jsCodeLines = []
+
+
+def indexStageTypes = stages.groupBy { it.type }
+log.info "Index pipeline stage types:\n" + indexStageTypes.collect { String key, def val -> "${val.size()} :: $key" }.join('\n')
 File indexOutFile = new File("index.javascript.js")
 indexOutFile.text = ''      // clear contents (if any)
 def jsIndexTypes = ['javascript-index', 'managed-js-index']
-jsIndexTypes.each {String type ->
-    List<Map<String, Object>> checkStages = indexStageTypes[type]
+
+jsIndexTypes.each { String stageType ->
+    List<Map<String, Object>> checkStages = indexStageTypes[stageType]
+
     checkStages.each {
         String label = it.label ?: 'unknown label'
-        queryOutFile << "// ---------------- $label --------------------\n"
-        String script = it.script ?: 'missing script??'
-        queryOutFile << script
-        queryOutFile << '\n'
+        String myscript = it.script
+
+        if (myscript) {
+            indexOutFile << "// ---------------- IDXP: $label --------------------\n"
+            indexOutFile << myscript
+            indexOutFile << '\n'
+
+            jsCodeLines << "// ---------------- IDXP: $label --------------------\n"
+            jsCodeLines.addAll(myscript.split('\n'))
+            jsCodeLines << "\n\n"
+
+        } else {
+            log.warn "No script in Index stage?? $it"
+        }
     }
 }
 
 
-def querystages = sourceFusionOjectsMap.queryPipelines.collect {it.stages}.flatten()
-def queryStageTypes = querystages.groupBy{it.type}
-log.info "Query pipeline stage types:\n" + queryStageTypes.collect {String key, def val -> "${val.size()} :: $key"}.join('\n')
-
-File queryOutFile = new File("query.javascript.js")
-queryOutFile.text = ''      // clear contents (if any)
-def jsTypes = ['javascript-query', 'managed-js-query']
-jsTypes.each {String type ->
-    List<Map<String, Object>> checkStages = queryStageTypes[type]
-    checkStages.each {
-        String label = it.label ?: 'unknown label'
-        queryOutFile << "// ---------------- $label --------------------\n"
-        String script = it.script ?: 'missing script??'
-        queryOutFile << script
-        queryOutFile << '\n'
-    }
-}
-log.info " Wrote file: ${queryOutFile.absolutePath}"
-log.info "done...?"
+List querystages = sourceFusionOjectsMap.queryPipelines.collect { it.stages }.flatten()
