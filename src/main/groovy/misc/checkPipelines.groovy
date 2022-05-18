b5bfdbbfc667c05d249bc71ce4946dfd4ce10b56
seanoc5 <seanoc5@gmail.com>
2022-05-18 10:19:21 -0400

    refactor index pipeline code, add query pipeline

--- src/main/groovy/misc/checkPipelines.groovy
+++ src/main/groovy/misc/checkPipelines.groovy
@@ -21,6 +21,39 @@ List<Map<String,Object>> indexPipelines = sourceFusionOjectsMap.indexPipelines
 def stages = indexPipelines.collect {it.stages}.flatten()
 def skipped = stages.findAll{it.skip}
 
-def stageTypes = stages.groupBy{it.type}
+def indexStageTypes = stages.groupBy{it.type}
+log.info "Index pipeline stage types:\n" + indexStageTypes.collect {String key, def val -> "${val.size()} :: $key"}.join('\n')
+File indexOutFile = new File("index.javascript.js")
+indexOutFile.text = ''      // clear contents (if any)
+def jsIndexTypes = ['javascript-index', 'managed-js-index']
+jsIndexTypes.each {String type ->
+    List<Map<String, Object>> checkStages = indexStageTypes[type]
+    checkStages.each {
+        String label = it.label ?: 'unknown label'
+        queryOutFile << "// ---------------- $label --------------------\n"
+        String script = it.script ?: 'missing script??'
+        queryOutFile << script
+        queryOutFile << '\n'
+    }
+}
 
+
+def querystages = sourceFusionOjectsMap.queryPipelines.collect {it.stages}.flatten()
+def queryStageTypes = querystages.groupBy{it.type}
+log.info "Query pipeline stage types:\n" + queryStageTypes.collect {String key, def val -> "${val.size()} :: $key"}.join('\n')
+
+File queryOutFile = new File("query.javascript.js")
+queryOutFile.text = ''      // clear contents (if any)
+def jsTypes = ['javascript-query', 'managed-js-query']
+jsTypes.each {String type ->
+    List<Map<String, Object>> checkStages = queryStageTypes[type]
+    checkStages.each {
+        String label = it.label ?: 'unknown label'
+        queryOutFile << "// ---------------- $label --------------------\n"
+        String script = it.script ?: 'missing script??'
+        queryOutFile << script
+        queryOutFile << '\n'
+    }
+}
+log.info " Wrote file: ${queryOutFile.absolutePath}"
 log.info "done...?"
