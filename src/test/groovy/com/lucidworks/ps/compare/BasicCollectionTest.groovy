6e50199fa097bf64f0c01ac710cb425a90aeff0a
seanoc5 <seanoc5@gmail.com>
2022-06-06 12:15:08 -0400

    add logic for xml comparison

--- src/test/groovy/com/lucidworks/ps/compare/BasicCollectionTest.groovy
+++ src/test/groovy/com/lucidworks/ps/compare/BasicCollectionTest.groovy
@@ -1,7 +1,6 @@
 package com.lucidworks.ps.compare
 
 import com.lucidworks.ps.upval.Helper
-import groovy.xml.XmlParser
 import spock.lang.Specification
 
 class BasicCollectionTest extends Specification {
@@ -36,22 +35,5 @@ class BasicCollectionTest extends Specification {
     }
 
 
-    def "Flatten demo schema files"() {
-        given:
-        XmlParser parser = new XmlParser()
-        def leftResource = getClass().getResourceAsStream('/templates/configsets/fusion-3.1.5/basic_configs/conf/managed-schema')
-        def rightResource = getClass().getResourceAsStream('templates/configsets/fusion-4.2.6/_default/conf/managed-schema')
-        def leftSchema = parser.parse(leftResource)
-        def rightSchema = parser.parse(rightResource)
-
-        when:
-        def flatLeft = Helper.flattenPlus(leftMap)
-        def flatRight = Helper.flattenPlus(rightMap)
-
-        then:
-        flatLeft.size() == 9
-        flatRight.size() == 11
-        flatLeft == flatRight
-    }
 
 }
