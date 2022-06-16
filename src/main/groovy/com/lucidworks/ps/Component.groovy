e7a1d499ad220db84ddf2c362b9ff3efbe639fd0
Sean O'Connor <seanoc5@gmail.com>
2022-06-16 06:37:07 -0400

    cleaning cruft, reorganizing

--- src/main/groovy/com/lucidworks/ps/Component.groovy
+++
@@ -1,4 +0,0 @@
-package com.lucidworks.ps
-
-class Component {
-}
--- src/main/groovy/config.groovy
+++
@@ -1,6 +0,0 @@
-// testing groovy config processing: configSlurper
-// todo - get something actually interesting in here
-datasources = [
-        "lucid.ldap-acls": "lucidworks.ldap-acls",
-]
-
