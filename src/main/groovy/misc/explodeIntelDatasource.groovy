4870c4eff9ae0ff1e0045555e13c937cfb5423aa
Sean O'Connor <seanoc5@gmail.com>
2022-07-06 12:52:37 -0400

    working to fix unparsable date format, switching from generic JsonOutput to JsonGenerator with custom options (dateFormat is ISO_Instant) format

--- src/main/groovy/misc/explodeIntelDatasource.groovy
+++ src/main/groovy/misc/explodeIntelDatasource.groovy
@@ -1,10 +1,8 @@
 package misc
 
-
 import com.lucidworks.ps.upval.FusionClientArgParser
 import groovy.cli.picocli.OptionAccessor
 import groovy.json.JsonGenerator
-import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import org.apache.log4j.Logger
 /**
@@ -24,24 +22,32 @@ exportFolder.mkdirs()
 
 // http://man.hubwiz.com/docset/Groovy.docset/Contents/Resources/Documents/groovy/json/JsonGenerator.Options.html
 def jsonDefaultOutput = new JsonGenerator.Options()
-.dateFormat('YYYY-MM-DD\'T\'HH:mm:ss.sssZ')
+        .dateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+//        .dateFormat('YYYY-MM-DD\'T\'HH:mm:ss.sssZ')
         .build()
 
 JsonSlurper slurper = new JsonSlurper()
 def dataSources = slurper.parse(srcFile)
-def brokenDataSources = dataSources.findAll {it.created.contains(' EDT ')}
+def brokenDataSources = dataSources.findAll { it.created.contains(' EDT ') }
 brokenDataSources.each {
     String name = it.id
     String type = it.type
     String filename = "${type}.${name}.json"
-    log.info "Datasource: $it"
-    String json = JsonOutput.toJson(it)
-    File outFile = new File(exportFolder, filename)
-    outFile.text = json
+    String cdate = it.created
+    if (cdate.contains('EDT')) {
+        Date now = new Date()
+//        Instant now = Instant.now()
+        it.created = now
+        it.modified = now
+    }
+//    log.debug "Datasource: $it"
+//    String json = JsonOutput.toJson(it)
+//    File outFile = new File(exportFolder, filename)
+//    outFile.text = json
 
     String json2 = jsonDefaultOutput.toJson(it)
-    outFile = new File(exportFolder, 'iso-' + filename)
+    outFile = new File(exportFolder, filename)
     outFile.text = json2
 
-    log.info "wrote file: ${outFile}"
+    log.info "wrote file: ${outFile.absolutePath}"
 }
