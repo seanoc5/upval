b586aba9490dcc3cbd243c96da82bdbff4186400
Sean O'Connor <seanoc5@gmail.com>
2022-07-07 09:38:13 -0400

    move Helper to Fusion-j use application default list of things to export
    TODO -- move export customization to BaseObject objects
    add collection/list handling for Jobs, links, and unknown

--- src/main/groovy/exportFusionAppToFileSystem.groovy
+++ src/main/groovy/exportFusionAppToFileSystem.groovy
@@ -1,3 +1,4 @@
+import com.lucidworks.ps.Helper
 import com.lucidworks.ps.model.BaseObject
 import com.lucidworks.ps.model.fusion.Application
 import com.lucidworks.ps.upval.ExportedAppArgParser
@@ -25,28 +26,41 @@ def jsonCustomOutput = new JsonGenerator.Options()
 if (!appZipFile?.canRead()) {
     throw new IllegalArgumentException("Could not find valid source file: ${options.source} (should be an app export zip file)")
 } else {
-    List<String> thingsToExport = "configsets collections dataSources indexPipelines queryPipelines parsers".split(' ')
-    thingsToExport = "collections indexPipelines queryPipelines parsers".split(' ')
+    List<String> thingsToExport = Application.DEFAULT_APP_OBJECTS
+//    thingsToExport = "collections indexPipelines queryPipelines parsers".split(' ')
 //    List<String> thingsToExport = FusionApplicationComparator.DEFAULTTHINGSTOCOMPARE
     Application app = new Application(appZipFile)
     thingsToExport.each { String typeName ->
         def exportable = app.getThings(typeName)
-        log.info "Export Things of type: $typeName (classname: ${exportable.getClass().simpleName})"
+        log.debug "Export Things of type: $typeName (classname: ${exportable.getClass().simpleName})"
 
         if (exportable instanceof Collection) {
-            log.info "\t\t$typeName) Collection to export: ${exportable.size()}"
+            log.info "$typeName) Collection to export: ${exportable.size()}"
             int i = 0
             exportable.each {   //String label, Collection collection ->
                 i++
                 String label
-                if(it instanceof Map && it.id){
-                    label = it.id
+                if(it instanceof Map){
+                    if(it.id) {
+                        label = it.id
+                    } else if(it.resource){         // handle JOBs here
+                        label = "${it.resource}"
+                        log.debug "$typeName) Handling label special case: $label"
+                    } else if(it.linkType){
+                        label = "${it.linkType}-${it.subject}-${it.object}"
+                        log.debug "$typeName) Handling label special case: $label"
+                    } else {
+                        log.warn "$typeName) unknown export label, going with counter of objects (probably bad for diff'ing)"
+                        label = "$i"
+                    }
+
                 } else {
-                    log.warn "export with index in name, can't find id attrib in object: ${it.getClass().simpleName}"
+                    label = "${i}"
+                    log.warn "$typeName) export with item counter in name rather than item id; can't find id attrib in object: ${it.getClass().simpleName}"
                 }
                 String jsonString = jsonCustomOutput.toJson(it)
                 String prettyJson = JsonOutput.prettyPrint(jsonString)
-                String outName = "${typeName}.${label}.json"
+                String outName = Helper.sanitizeFilename("${typeName}.${label}.json")
                 // todo -- get better naming for list items...
                 File outfile = new File(exportFolder, outName)
                 outfile.text = prettyJson
