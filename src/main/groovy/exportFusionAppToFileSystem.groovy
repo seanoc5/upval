a3e5e5ff98b836f449ca48240d3a070095c83f36
SeanO'Connor <sean.oconnor@lucidworks.com>
2022-07-10 15:54:42 -0400

    updated functionality for exporting fusion objects

--- src/main/groovy/exportFusionAppToFileSystem.groovy
+++ src/main/groovy/exportFusionAppToFileSystem.groovy
@@ -1,11 +1,11 @@
 import com.lucidworks.ps.Helper
+import com.lucidworks.ps.clients.ExportedAppArgParser
 import com.lucidworks.ps.model.BaseObject
 import com.lucidworks.ps.model.fusion.Application
-import com.lucidworks.ps.clients.ExportedAppArgParser
 import groovy.cli.picocli.OptionAccessor
 import groovy.json.JsonGenerator
-import groovy.json.JsonOutput
 import org.apache.log4j.Logger
+
 /**
  * test script to proof-out the process of exporting "all the things" from an fusion app (assuming app export zip as source)
  */
@@ -16,6 +16,7 @@ log.info "start script ${this.class.name}..."
 OptionAccessor options = ExportedAppArgParser.parse(this.class.name, args)
 File appZipFile = new File(options.source)
 File exportFolder = options.exportDir ? new File(options.exportDir) : null
+boolean groupedExport = options.grouped
 
 // http://man.hubwiz.com/docset/Groovy.docset/Contents/Resources/Documents/groovy/json/JsonGenerator.Options.html
 def jsonCustomOutput = new JsonGenerator.Options()
@@ -27,63 +28,42 @@ if (!appZipFile?.canRead()) {
     throw new IllegalArgumentException("Could not find valid source file: ${options.source} (should be an app export zip file)")
 } else {
     List<String> thingsToExport = Application.DEFAULT_APP_OBJECTS
-//    thingsToExport = "collections indexPipelines queryPipelines parsers".split(' ')
-//    List<String> thingsToExport = FusionApplicationComparator.DEFAULTTHINGSTOCOMPARE
     Application app = new Application(appZipFile)
     thingsToExport.each { String typeName ->
         def exportable = app.getThings(typeName)
-        log.debug "Export Things of type: $typeName (classname: ${exportable.getClass().simpleName})"
-
-        if (exportable instanceof Collection) {
-            log.info "$typeName) Collection to export: ${exportable.size()}"
-            int i = 0
-            exportable.each {   //String label, Collection collection ->
-                i++
-                String label
-                if(it instanceof Map){
-                    if(it.id) {
-                        label = it.id
-                    } else if(it.resource){         // handle JOBs here
-                        label = "${it.resource}"
-                        log.debug "$typeName) Handling label special case: $label"
-                    } else if(it.linkType){
-                        label = "${it.linkType}-${it.subject}-${it.object}"
-                        log.debug "$typeName) Handling label special case: $label"
-                    } else {
-                        log.warn "$typeName) unknown export label, going with counter of objects (probably bad for diff'ing)"
-                        label = "$i"
-                    }
-
-                } else {
-                    label = "${i}"
-                    log.warn "$typeName) export with item counter in name rather than item id; can't find id attrib in object: ${it.getClass().simpleName}"
+        if(exportable) {
+            log.info "---------------- Export Things of type: $typeName (classname: ${exportable.getClass().simpleName}) ----------------"
+            File outFolder = exportFolder
+            if (exportable instanceof BaseObject) {
+                if (groupedExport) {
+                    outFolder = Helper.getOrMakeDirectory(exportFolder, typeName)
                 }
-                String jsonString = jsonCustomOutput.toJson(it)
-                String prettyJson = JsonOutput.prettyPrint(jsonString)
-                String outName = Helper.sanitizeFilename("${typeName}.${label}.json")
-                // todo -- get better naming for list items...
-                File outfile = new File(exportFolder, outName)
-                outfile.text = prettyJson
-                log.debug "Wrote file (${outfile.absolutePath}) with collection (${it.getClass().simpleName}) size: ${it.size()}"
+                ((BaseObject) exportable).export(outFolder)
+                log.info "exported BaseObject: $exportable"
+            } else if (exportable instanceof Collection) {
+                Collection exp = exportable
+                log.warn "Exportable (${exp.size()}) is of type: (${exp.getClass().simpleName}) "
+                exportable.each {
+
+                }
+            } else if (exportable instanceof Map) {
+                log.info "\t\t$typeName) Map to export: ${exportable.keySet().size()}"
+                exportable.each { String label, def collection ->
+                    String s = jsonCustomOutput.toJson(collection)
+                    String outName = "${typeName}.${label}.json"
+                    File outfile = new File(exportFolder, outName)
+                    outfile.text = s
+                    log.info "Wrote file (${outfile.absolutePath}) with Map (${collection.getClass().simpleName}) size: ${collection.size()}"
+                }
+
+            } else {
+                log.warn "$typeName) UNKNOWN type: $typeName: $exportable"
             }
-            log.debug "finished exporting collection type: $typeName."
-        } else if (exportable instanceof Map) {
-            log.info "\t\t$typeName) Map to export: ${exportable.keySet().size()}"
-            exportable.each { String label, def collection ->
-                String s = jsonCustomOutput.toJson(collection)
-                String outName = "${typeName}.${label}.json"
-                File outfile = new File(exportFolder, outName)
-                outfile.text = s
-                log.info "Wrote file (${outfile.absolutePath}) with Map (${collection.getClass().simpleName}) size: ${collection.size()}"
-            }
-        } else if (exportable instanceof BaseObject) {
-            ((BaseObject) exportable).export(exportFolder)
-            log.info "exported BaseObject: $exportable"
         } else {
-            log.warn "$typeName) UNKNOWN type: $typeName: $exportable"
+            log.warn "Tried getting things of type: $typeName, but found nothing... skipping..."
         }
     }
-
+    log.debug "Done iterating types: $thingsToExport"
 }
 
 log.info "done...?"
