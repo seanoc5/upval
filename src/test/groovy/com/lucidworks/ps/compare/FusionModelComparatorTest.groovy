94795fc059d85a2260a2e33ec92e267567cb35c4
Sean O'Connor <seanoc5@gmail.com>
2022-07-12 11:36:40 -0400

    start fresh with FusionModelComparator, remove extra code, revisit

---
+++ src/test/groovy/com/lucidworks/ps/compare/FusionModelComparatorTest.groovy
@@ -0,0 +1,29 @@
+package com.lucidworks.ps.compare
+
+import com.lucidworks.ps.model.fusion.Application
+import spock.lang.Specification
+
+/**
+ * @author :    sean
+ * @mailto :    seanoc5@gmail.com
+ * @created :   7/12/22, Tuesday
+ * @description:
+ */
+
+class FusionModelComparatorTest extends Specification {
+    def "Compare test app and slightly modified test app"() {
+        given:
+        File leftAppZip = new File(getClass().getResource('/apps/test.f5.partial.zip').toURI())
+        Application leftApp = new Application(leftAppZip)
+
+        File rightAppZip = new File(getClass().getResource('/apps/test.f5.partial.modified.zip').toURI())
+        Application rightApp = new Application(rightAppZip)
+
+        when:
+        FusionModelComparator comparator = new FusionModelComparator(leftApp, rightApp, FusionModelComparator.DEFAULTTHINGSTOCOMPARE, '.*(created|modified)')
+
+        then:
+        comparator.collectionComparisons.size() == 10
+
+    }
+}
