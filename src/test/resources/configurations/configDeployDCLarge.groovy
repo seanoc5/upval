8978abd7a3d53b93126fca8646a6f6c2763e1c28
SeanO'Connor <sean.oconnor@lucidworks.com>
2022-08-01 20:41:50 -0400

    adding pointers to where to begin understanding the code. Including pointers to "current" tests and hopefully avoiding time spent on outdated code/approaches.

--- src/test/resources/configurations/configDeployDCLarge.groovy
+++ src/test/resources/configurations/configDeployDCLarge.groovy
@@ -8,9 +8,10 @@ rules.copy = [
 ]
 
 // potentially keep blob and filesystem renames separate, but that may not be necessary. consider merging the rules below into the rules above.
+// note: `namePattern: ~/TYPEAH\w+\b/` is an unnecessarily-complex pattern, but included to show an example of the Pattern operator, slashy strings, an regex find (no regex replace yet...)
 rules.rename = [
         [namePattern: 'DC_Large', replacement: 'AcmeDigicommerce'],
-        [namePattern: ~/TYPEAHEAD/, replacement: 'acme_ta'],
+        [namePattern: ~/TYPEAH\w+\b/, replacement: 'acme_ta'],
         [namePattern: 'RESPONSE_TA', replacement: 'response_ta'],
         [namePattern: 'CAT_ID_MAPPING', replacement: 'category_mapping'],
 ]
