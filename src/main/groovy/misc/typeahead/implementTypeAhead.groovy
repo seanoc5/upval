783345926f6ba0d2c66452b585e6e328aea0e1c2
Sean O'Connor <seanoc5@gmail.com>
2022-08-09 11:38:56 -0400

    implement getting names from commandline and overriding config file values (with default fallback from taName)

--- src/main/groovy/misc/typeahead/implementTypeAhead.groovy
+++ src/main/groovy/misc/typeahead/implementTypeAhead.groovy
@@ -15,22 +15,33 @@ import java.nio.file.Paths
  */
 final Logger log = Logger.getLogger(this.class.name);
 
-log.info "Starting ${this.class.name}..."
+log.info "Starting ${this.class.name} with args: ${args}..."
 
 OptionAccessor options = FusionClientArgParser.parse(this.class.name, args)
 Path cfgUrl = Paths.get(options.config)
 File f = new File("/home/sean/work/lucidworks/upval/src/main/resources/configs/configTypeAhead.groovy")
 ConfigObject config = new ConfigSlurper().parse(f.toURI().toURL())
 log.info "Config: $config"
+String appName = options.appName
+String taName = config.taName
 
 FusionClient fusionClient = new FusionClient(options)
+
+def taCollection = fusionClient.getCollection(appName, taName)
+if(taCollection){
+    log.warn "Collection: $taName already exists, not recreating..."
+} else {
+    fusionClient.createCollection(taName)
+}
 
 config.blobs.each {String key, Object val ->
     String path = val.path
     String src = val.source
-    File blobFile = new File(src)
+    Path blobFile = Paths.get(src)
     log.info "blob $key"
-    def blobUpdate = fusionClient.blobUpload(path, blobFile)
+    String type = val.type
+    def blobUpdate = fusionClient.blobUpload(appName, path, type, blobFile)
+    log.info "result: $blobUpdate"
 }
 
 
