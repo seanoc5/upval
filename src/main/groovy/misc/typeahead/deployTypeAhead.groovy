c43c2edca52e37c185e7d071ff0efd5cff112c02
Sean O'Connor <seanoc5@gmail.com>
2022-08-11 16:38:12 -0400

    minor changes

--- src/main/groovy/misc/typeahead/deployTypeAhead.groovy
+++ src/main/groovy/misc/typeahead/deployTypeAhead.groovy
@@ -1,8 +1,9 @@
 package misc.typeahead
 
+import com.lucidworks.ps.clients.DeployArgParser
 import com.lucidworks.ps.clients.FusionClient
-import groovy.json.JsonSlurper
-import groovy.xml.XmlParser
+import com.lucidworks.ps.clients.FusionResponseWrapper
+import groovy.cli.picocli.OptionAccessor
 import org.apache.log4j.Logger
 
 import java.nio.file.Path
@@ -13,61 +14,84 @@ import java.nio.file.Paths
  * @created :   8/4/22, Thursday
  * @description:
  */
-final Logger log = Logger.getLogger(this.class.name);
-
+final Logger log = Logger.getLogger(this.class.name)
 log.info "Starting ${this.class.name} with args: ${args}..."
 
-/*OptionAccessor options = FusionClientArgParser.parse(this.class.name, args)
-URL configUrl = getClass().getResource(options.config)
 
+// ------------------ Configuration -------------------
+OptionAccessor options = DeployArgParser.parse(this.class.name, args)
+URL configUrl = getClass().getResource(options.config)
 String appName = options.appName
 String taName = options.taName
 ConfigSlurper configSlurper = new ConfigSlurper()
-Map bindingMap=[appName:appName]
-if(taName){
+Map bindingMap = [appName: appName]
+if (taName) {
     bindingMap['taName'] = taName
     log.info "Found typeahead name from CLI/options: $taName -- set binding to override config file...."
 }
 configSlurper.setBinding(bindingMap)
 ConfigObject config = configSlurper.parse(configUrl)
-log.info "Config: $config"*/
-File pkgSource = new File(getClass().getResource('/typeahead/').toURI())
-
-log.info "File f: ${pkgSource.absolutePath}"
-
-File xmlComponentSrc = new File(pkgSource,'Features_TYPEAHEAD_DW_F5.4_v4_component-def_TYPEAHEAD_DW_F5.4_v4.xml')
-File jsonVariablesSrc = new File(pkgSource,'Features_TYPEAHEAD_DW_F5.4_v4_variables.json')
-File jsonObjectsSrc = new File(pkgSource,'Features_TYPEAHEAD_DW_F5.4_v4_Feature_TYPEAHEAD_DW_F5.4_v4_objects.json')
-//Map taCollectionDef = config.collections.typeahead
-
-JsonSlurper jsonSlurper = new JsonSlurper()
-Map jsonVariables = jsonSlurper.parse(jsonVariablesSrc)
-Map jsonObjects = jsonSlurper.parse(jsonObjectsSrc)
-
-XmlParser xmlParser = new XmlParser()
-def xmlComponents = xmlParser.parse(xmlComponentSrc)
-
-log.info "What do we have?"
+log.info "ConfigSlurper using: $configUrl"
+log.debug "ConfigObject: $config"
 
 
-
+// ------------------ Fusion Client -------------------
 FusionClient fusionClient = new FusionClient(options)
-def taCollection = fusionClient.getCollection(appName, taName)
-if(taCollection){
-    log.warn "Collection: $taName already exists, not recreating..."
+
+
+// ------------------ Blobs -------------------
+def existingBlobs
+FusionResponseWrapper frw = fusionClient.getBlobs()
+if (frw.wasSuccess()) {
+    existingBlobs = frw.parsedList
 } else {
-    fusionClient.createCollection(taName, taCollectionDef, appName, false)
+    throw new IllegalArgumentException("Problem with call for blobs: $frw")
+}
+config.blobs.each { String key, Object val ->
+    def destinationBlob = existingBlobs.find { it.path == val.path }
+    if (destinationBlob) {
+        log.info "Destination blob already exists, not re-uploading: key: $key --blob:$val"
+    } else {
+        String path = val.path
+        String src = val.source
+        Path blobFile = Paths.get(getClass().getResource(src).toURI())
+        log.info "blob $key -- path: ${blobFile.toString()} -> abs path: ${blobFile.toAbsolutePath()}"
+        String type = val.type
+        def blobUpdate = fusionClient.blobUpload(appName, path, type, blobFile)
+        log.info "result: $blobUpdate"
+    }
 }
 
-config.blobs.each {String key, Object val ->
-    String path = val.path
-    String src = val.source
-    Path blobFile = Paths.get(src)
-    log.info "blob $key"
-    String type = val.type
-    def blobUpdate = fusionClient.blobUpload(appName, path, type, blobFile)
-    log.info "result: $blobUpdate"
+// ------------------ Collections -------------------
+def existingCollections = fusionClient.getCollections(appName)
+if (existingCollections) {
+    config.collections.each { String label, Map collectionConfig ->
+        String destId = collectionConfig.id
+        log.info "process Coll ID: $destId (label:$label) -> $collectionConfig"
+        def taCollection = existingCollections.find { it.id == destId }
+        if (taCollection) {
+            log.warn "Collection: $taName already exists, not recreating : ${taCollection}"
+        } else {
+            log.info "Create new collection ($destId) in app: $appName -- solrParams: $collectionConfig"
+            FusionResponseWrapper frwBCollection = fusionClient.createCollection(taName, collectionConfig, appName, false)
+            log.info "FRW: $frwBCollection"
+        }
+    }
 }
 
+// ------------- DataSources --------------------
+def existingDatasources = fusionClient.getDataSources(appName)
+config.dataSources.each { String label, Map dsConfig ->
+    String dsId = dsConfig.id
+    log.info "Config Package Datasource ($label) with id: $dsId and keys: ${dsConfig.keySet()}"
+    def destinationDS = existingDatasources.find {it.id == dsId }
+    if (destinationDS) {
+        log.info "Datasource ($label :: $dsId) already exists, skipping upload: $dsConfig"
+    } else {
+        log.info "Upload datasource: ($label :: $dsId)"
+        FusionResponseWrapper frwDS = fusionClient.createDataSource(dsConfig, appName)
+        log.info "\t\tDatasource ($dsId) response: $frwDS"
+    }
+}
 
 log.info "Done...?"
