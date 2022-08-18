179d8571d89f0d0c377ca23a08b39bb3cdb0972c
Sean O'Connor <seanoc5@gmail.com>
2022-08-18 19:09:27 -0400

    addind test data for experimenting with ConfigSlurperTest

--- src/test/resources/configs/simpleConfig.groovy
+++ src/test/resources/configs/simpleConfig.groovy
@@ -4,11 +4,12 @@ import groovy.json.JsonSlurper
 taName = "${taName ?: 'myTypeahead'}"
 appName = "${appName ?: 'defaultAppName'}"
 baseId = "${appName}_$taName"
-//objectsUrl = getClass().getResource('/resources/components')
-//fooFile = new File(objectsUrl.toURI())
-//println(fooFile.absolutePath)
+// can't seem to get access to /resources dynamically? only to /classes/...
+objectsUrl = getClass().getResource('/')
+fooFile = new File(objectsUrl.toURI())
+println(fooFile.absolutePath)
 
-sourceObjects = new File('/Users/sean/work/lucidworks/upval/src/test/resources/components/ta-objects.json')
+sourceObjects = new File('/home/sean/work/lucidworks/upval/src/test/resources/components/simpleObjects.json')
 objects = new JsonSlurper().parse(sourceObjects.toURL())
 println(objects)
 
