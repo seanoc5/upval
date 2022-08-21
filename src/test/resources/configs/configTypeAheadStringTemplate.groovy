65c2b0c3059a90b2212b046706d8fccddca80173
SeanO'Connor <sean.oconnor@lucidworks.com>
2022-08-21 17:31:59 -0400

    minor content changes -- this approach fails currently with dollar signs in the content

--- src/test/resources/configs/configTypeAheadStringTemplate.groovy
+++ src/test/resources/configs/configTypeAheadStringTemplate.groovy
@@ -1,23 +1,32 @@
+import com.lucidworks.ps.transform.JsonObject
 import groovy.json.JsonSlurper
 
 variables {
-    ZKHOST: "myzk-0.myzk-headless:2181,myzk-1.myzk-headless:2181,myzk-2.myzk-headless:2181"
-    SIGNALS_AGGR_COLL: "SIGNALS_AGGR_COLLECTION"
-    FEATURE_NAME: "TYPEAHEAD_DW"
-    TYPE_FIELD_1: "TYPE_FIELD_1"
-    TYPE_FIELD_2: "TYPE_FIELD_2"
-    TYPE_FIELD_3: "TYPE_FIELD_3"
-    TYPE_FIELD_4: "TYPE_FIELD_4"
-    TYPE_FIELD_5: "TYPE_FIELD_5"
-    APP: "Components"
-    COLLECTION: "COLLECTION"
+    FEATURE_NAME = "MyTypeAhead"
+    APP = "MyApp"
+    COLLECTION = "MyAppColl"
+    ZKHOST = "myzk-0.myzk-headless=2181,myzk-1.myzk-headless=2181,myzk-2.myzk-headless=2181"
+    SIGNALS_AGGR_COLL = "SIGNALS_AGGR_COLLECTION"
+    TYPE_FIELD_1 = "TYPE_FIELD_1"
+    TYPE_FIELD_2 = "TYPE_FIELD_2"
+//    TYPE_FIELD_3= "TYPE_FIELD_3"
+//    TYPE_FIELD_4= "TYPE_FIELD_4"
+//    TYPE_FIELD_5= "TYPE_FIELD_5"
+    baseId = "${APP}_${FEATURE_NAME}"
+    jsSource = new File('src/test/resources/components/typeahead/excludeUnwantedTerms.js').text
+    println "${JsonObject.escapeSource(jsSource)}"
+//    jsUnwantedTerms = JsonObject.escapeSource(jsSource)
+
+    jsUnwantedTerms = """
+function (doc){
+    return doc;
 }
+"""
+}
+//testFile = new File('.')
+//println("Running from: ${testFile.absolutePath}")
+//println "Variables: ${variables.collect{"\n\t\t$it"}}"
 
-objectsJson =new File('/Users/sean/work/lucidworks/upval/src/test/resources/components/ta-objects.json').text
-String output =  new groovy.text.SimpleTemplateEngine().createTemplate(objectsJson).make(variables).toString()
-Map map = new JsonSlurper().parseText(output)
-
-
-//objects = new JsonSlurper().parse(sourceObjects.toURL())
-//println(objects)
-
+indexJson = new File('src/test/resources/components/typeahead/indexpipeline.short-test.json').text
+output = new groovy.text.SimpleTemplateEngine().createTemplate(indexJson).make(variables).toString()
+map = new JsonSlurper().parseText(output)
