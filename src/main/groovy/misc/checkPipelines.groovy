15bbb9ca33795e6b2de737cbe3747901bb2f0538
Sean O'Connor <seanoc5@gmail.com>
2022-08-25 13:24:58 -0400

    improving extracting grouping/tagging (and solr saving) of javascript from app exports

--- src/main/groovy/misc/checkPipelines.groovy
+++ src/main/groovy/misc/checkPipelines.groovy
@@ -1,17 +1,26 @@
 package misc
 
-//import com.lucidworks.ps.fusion.Application
-import com.lucidworks.ps.js.PipelineJsAnalyzer
+import com.lucidworks.ps.Helper
+import com.lucidworks.ps.clients.ExportedAppArgParser
 import com.lucidworks.ps.model.fusion.Application
+import com.lucidworks.ps.model.fusion.Javascript
 import groovy.cli.picocli.OptionAccessor
 import org.apache.log4j.Logger
+import org.apache.solr.client.solrj.SolrClient
+import org.apache.solr.client.solrj.impl.HttpSolrClient
+import org.apache.solr.client.solrj.response.UpdateResponse
+import org.apache.solr.common.SolrInputDocument
+
+//import com.lucidworks.ps.fusion.Application
 
 Logger log = Logger.getLogger(this.class.name)
 
 OptionAccessor options = ExportedAppArgParser.parse(this.class.name, args)
 
 log.info "start script ${this.class.name}..."
+
 //FusionClient fusionClient = new FusionClient(options)
+
 File srcFile = new File(options.source)
 if(srcFile && srcFile.canRead()){
     log.info "Source file: ${srcFile.absoluteFile}"
@@ -20,45 +29,45 @@ if(srcFile && srcFile.canRead()){
 Application application = new Application(srcFile)
 Map fusionParsedMap = application.exportedObjectsSourceMap
 
-//todo add Application method: getJavascriptThings()?
-Map<String, Object> objects = fusionParsedMap.objects
-List<Map<String, Object>> foo = objects.indexPipelines
-def indexPipelines = application.indexPipelines
+//Map<String, Map<String, Object>> idxp = application.indexPipelines.pipelines.collectEntries {k, v -> v.each {it. = 'index'; return [k, v] }
+//Map<String, Map<String, Object>> idxStages = application.indexPipelines.pipelineStagesMap
 
-def stages = indexPipelines.collect { it.stages }.flatten()
-def jsStages = PipelineJsAnalyzer.collectJsStages(stages)
+Map<String, List<Javascript>> idxJsStages = application.indexPipelines.javascriptStages
+//Map<String, Map<String, Object>> qryStages = application.queryPipelines.pipelineStagesMap
+Map<String, List<Javascript>> qryJsStages = application.queryPipelines.javascriptStages
+//def qstgTypeGrouped = qryp.values()*.stages*.type.flatten().groupBy {it}
 
-// bulk list (all index & query JS stage) of javascript code lines, will be filtered and analyzed at the end
-List<String> jsCodeLines = []
+File exportDir = Helper.getOrMakeDirectory(options.exportDir)
+def allJSStages = idxJsStages + qryJsStages
 
+String host = 'newmac'
+int port = 8983
+String collection = 'lucy'
+String baseUrl = "http://$host:$port/solr/$collection"
+//SolrClient solrClient = new HttpSolrClient(baseUrl)
+SolrClient solrClient = new HttpSolrClient.Builder(baseUrl).build();
 
-def indexStageTypes = stages.groupBy { it.type }
-log.info "Index pipeline stage types:\n" + indexStageTypes.collect { String key, def val -> "${val.size()} :: $key" }.join('\n')
-File indexOutFile = new File("index.javascript.js")
-indexOutFile.text = ''      // clear contents (if any)
-def jsIndexTypes = ['javascript-index', 'managed-js-index']
-
-jsIndexTypes.each { String stageType ->
-    List<Map<String, Object>> checkStages = indexStageTypes[stageType]
-
-    checkStages.each {
-        String label = it.label ?: 'unknown label'
-        String myscript = it.script
-
-        if (myscript) {
-            indexOutFile << "// ---------------- IDXP: $label --------------------\n"
-            indexOutFile << myscript
-            indexOutFile << '\n'
-
-            jsCodeLines << "// ---------------- IDXP: $label --------------------\n"
-            jsCodeLines.addAll(myscript.split('\n'))
-            jsCodeLines << "\n\n"
-
-        } else {
-            log.warn "No script in Index stage?? $it"
-        }
+List< SolrInputDocument> sidList = []
+allJSStages.each { String key, List<Javascript> jsList ->
+    jsList.each { Javascript jsStage ->
+        String label = jsStage.label
+        File f = new File(exportDir, label + ".js")
+        f.text = jsStage.script
+        log.info "$key) (${jsStage.lines.size()}) lines -> ${f.absolutePath}"
+        SolrInputDocument sid = jsStage.toSolrInputDocument()
+        sidList << sid
     }
 }
 
+UpdateResponse resp = solrClient.add(sidList, 1000)
+log.info "Sent (${sidList.size()}) solr docs (JS STages) to solr, response: $resp"
 
-List querystages = fusionParsedMap.queryPipelines.collect { it.stages }.flatten()
+
+//log.info "Index pipeline stage types:\n" + indexStageTypes.collect { String key, def val -> "${val.size()} :: $key" }.join('\n')
+//File indexOutFile = new File("index.javascript.js")
+//indexOutFile.text = ''      // clear contents (if any)
+//def jsIndexTypes = ['javascript-index', 'managed-js-index']
+//
+//jsIndexTypes.each { String stageType ->
+//    List<Map<String, Object>> checkStages = indexStageTypes[stageType]
+//}
