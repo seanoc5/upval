2c1d89819c00a0bba1d8d2aaf0f94621b07f7766
Sean O'Connor <seanoc5@gmail.com>
2023-05-23 08:11:19 -0400

    fixing file path issues in ConfigSlurperTest todo -- more updates needed...

--- src/test/resources/configs/simpleConfig.groovy
+++ src/test/resources/configs/simpleConfig.groovy
@@ -6,13 +6,18 @@ appName = "${appName ?: 'defaultAppName'}"
 baseId = "${appName}_$taName"
 username = "${username ?: 'testUser'}"
 // can't seem to get access to /resources dynamically? only to /classes/...
-objectsUrl = getClass().getResource('/')
-println "Resources root url: $objectsUrl"
-fooFile = new File(objectsUrl.toURI())
+rootObjectsUrl = getClass().getResource('/')
+println "Resources root url: $rootObjectsUrl"
+fooFile = new File(rootObjectsUrl.toURI())
 println("Resources root file path: ${fooFile.absolutePath}")
 
-sourceObjects = new File('/home/sean/work/lucidworks/upval/src/test/resources/components/simpleObjects.json')
-objects = new JsonSlurper().parse(sourceObjects.toURL())
+objectsUrl = getClass().getResource('/components/simpleObjects.json')
+println objectsUrl
+
+//sourceObjects = new File(objectsUrl)
+//sourceObjects = new File('/home/sean/work/lucidworks/upval/src/test/resources/components/simpleObjects.json')
+objects = new JsonSlurper().parse(objectsUrl)
+//objects = new JsonSlurper().parse(sourceObjects.toURL())
 //println(objects)
 
 
